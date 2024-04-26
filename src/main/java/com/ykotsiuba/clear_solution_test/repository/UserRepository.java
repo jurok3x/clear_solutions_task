2f217fa29d264570f026e65dc089bed726e8721a
ykotsiuba <jurok3x@gmail.com>
2024-04-26 15:07:57 +0300

    Updated repository.

--- src/main/java/com/ykotsiuba/clear_solution_test/repository/UserRepository.java
+++ src/main/java/com/ykotsiuba/clear_solution_test/repository/UserRepository.java
@@ -2,8 +2,16 @@ package com.ykotsiuba.clear_solution_test.repository;
 
 import com.ykotsiuba.clear_solution_test.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    List<User> findAllByBirthDateBetween(
+            LocalDate start,
+            LocalDate end);
 }
