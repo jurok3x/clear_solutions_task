42ef9c48c62b1bbe91a34f37655d8a00a51adf02
ykotsiuba <jurok3x@gmail.com>
2024-04-26 15:07:11 +0300

    Updated User mapper.

--- src/main/java/com/ykotsiuba/clear_solution_test/mapper/UserMapper.java
+++ src/main/java/com/ykotsiuba/clear_solution_test/mapper/UserMapper.java
@@ -1,12 +1,19 @@
 package com.ykotsiuba.clear_solution_test.mapper;
 
+import com.ykotsiuba.clear_solution_test.dto.SaveUserRequestDTO;
 import com.ykotsiuba.clear_solution_test.dto.UserDTO;
 import com.ykotsiuba.clear_solution_test.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserDTO toDTO(User user);
+    UserDTO fromEntityToDTO(User user);
+
+    @Mapping(target = "id", ignore = true)
+    User fromSaveRequestToEntity(SaveUserRequestDTO requestDTO);
+
+    User fromDTOToEntity(UserDTO userDTO);
 
 }
