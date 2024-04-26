a765c663518dd32a3b46786ccde5c174b42136c5
ykotsiuba <jurok3x@gmail.com>
2024-04-26 15:09:55 +0300

    Added delete response dto.

---
+++ src/main/java/com/ykotsiuba/clear_solution_test/dto/DeleteUserResponseDTO.java
@@ -0,0 +1,14 @@
+package com.ykotsiuba.clear_solution_test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteUserResponseDTO {
+    private String message;
+}
